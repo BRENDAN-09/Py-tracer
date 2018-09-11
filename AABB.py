944e128a96472d9825f298c1f6d510fc291f02f7
Brendan Wilson <brendan@Brendans-MacBook-Air.local>
2018-09-11 19:21:29 +1000

    box subdivision

--- AABB.py
+++ AABB.py
@@ -3,19 +3,23 @@
 from Vector3 import Vec3
 from Ray import Ray
 from Triangle import Triangle
+import json
 
 
 class AABB():
     def __init__(self, v1, v2):
         self.info = [v1, v2]
 
+
     def intersect(self, r):
         lo = -float("Inf")
         hi = float("Inf")
 
         for i in ["x", "y", "z"]:
-            dimLo = (getattr(self.info[0], i) - getattr(r.o, i)) / getattr(r.d, i)
-            dimHi = (getattr(self.info[1], i) - getattr(r.o, i)) / getattr(r.d, i)
+            dimLo = (getattr(self.info[0], i) -
+                     getattr(r.o, i)) / getattr(r.d, i)
+            dimHi = (getattr(self.info[1], i) -
+                     getattr(r.o, i)) / getattr(r.d, i)
 
             # Swap so that dimHi > dimLo
             if dimLo > dimHi:
@@ -48,9 +52,27 @@ class AABB():
                 return False
         return True
 
+    def subDivide(self):
+        a = self.info[0]
+        g = self.info[1]
+        b = Vec3(g.x, a.y, a.z)
+        c = Vec3(g.x, a.y, g.z)
+        d = Vec3(a.x, a.y, g.z)
+        e = Vec3(a.x, g.y, a.z)
+        f = Vec3(b.x, g.y, b.z)
+        h = Vec3(d.x, g.y, g.z)
+        m = self.info[0] + ((self.info[1] - self.info[0]) ^ 0.5)
+        boxes = []
+        for i in [a, b, c, d, e, f, g, h]:
+            boxes.append(AABB(i, m))
+        return boxes
 
-aabb = AABB(Vec3(0, 0, 0), Vec3(1, 1, 1))
-r = Ray(orig=Vec3(200, 2, 2), dir=Vec3(1, 1, 1))
+
+"""aabb = AABB(Vec3(1, 1, 1), Vec3(0, 0, 0))
+r = Ray(orig=Vec3(2, 2, 2), dir=Vec3(-1, -1, -1))
 i = aabb.containsPoint(Vec3(0.5, 1.2, 0.5))
-t = Triangle(Vec3(0.1, 3, 0.1), Vec3(0.3, 0.3, 0.2), Vec3(0.8, 0.6, 0.2), "gre")
-print(aabb.containsTri(t))
+t = Triangle(Vec3(0.1, 3, 0.1), Vec3(
+    0.3, 0.3, 0.2), Vec3(0.8, 0.6, 0.2), "gre")
+b = aabb.subDivide()
+f = open("car.json", "w")
+print(b)"""
